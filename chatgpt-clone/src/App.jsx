279770894b871282be7062e0c390b5a2ebcca986
David-T10 <54846690+David-T10@users.noreply.github.com>
2023-01-19 21:25:49 +0000

    moving main folderrs over
    beginnings of image gen

---
+++ chatgpt-clone/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    console.log(res.data.data[0].url);
+  };
+
+  return (
+    <div className="main">
+      <h3>Generate Image using OpenAI </h3>
+      <input
+        className="inputbox"
+        placeholder="Type anything..."
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button onClick={generateImage}>Generate Image</button>
+    </div>
+  );
+}
+
+export default App;
